8f44afa62f921ef55c865a833c96f68258f26d04
Daniel <daniel.e.saldana@gmail.com>
2016-10-21 15:17:02 -0700

    Initial project commit

---
+++ Assets/Scripts/BodyController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BodyController : MonoBehaviour 
+{
+	public float inputDelay = 0.1f;
+	public float forwardVel = 12;
+	public float rotateVel = 100;
+
+	Quaternion targetRotation;
+	Rigidbody rb;
+	float forwardInput, turnInput;
+
+	public Quaternion TargetRotation
+	{
+		get{ return targetRotation; }
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		targetRotation = transform.rotation;
+		if (GetComponent<Rigidbody>())
+			rb = GetComponent<Rigidbody>();
+		else 
+				Debug.LogError("Need Rigidbody");
+			
+		forwardInput = turnInput = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		GetInput ();
+		Turn ();
+	}
+
+	void FixedUpdate()
+	{
+		Run ();
+	}
+
+	void GetInput()
+	{
+		forwardInput = Input.GetAxis("Vertical");
+		turnInput = Input.GetAxis ("Horizontal");
+	}
+
+	void Run()
+	{
+		if (Mathf.Abs (forwardInput) > inputDelay) 
+		{
+			rb.velocity = transform.forward * forwardInput * forwardVel;
+		} else
+			rb.velocity = Vector3.zero;
+	}
+
+	void Turn()
+	{
+		if (Mathf.Abs (turnInput) > inputDelay) 
+		{
+			targetRotation *= Quaternion.AngleAxis (rotateVel * turnInput * Time.deltaTime, Vector3.up);
+		}
+		transform.rotation = targetRotation;
+	}
+
+}
