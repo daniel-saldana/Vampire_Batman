dc3b23d724e941092892d0242dee101c3af4f034
Oscar Zldana <oscar.zaldana@yahoo.com>
2016-10-24 18:11:30 -0700

    Auto stash for cherrypick of "Soar is working though i want to change a few things"

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour 
 {
-	
+	public Transform target;
+	public  float lookSmooth = 0.09f;
+	public Vector3 offsetFromTarget = new Vector3(0,6,-8);
+	public float xTilt = 10;
+
+	Vector3 destination = Vector3.zero;
+	BodyController bodCon;
+	float rotateVel = 0;
+
 	// Use this for initialization
 	void Start () 
 	{
-	
+		SetCameraTarget (target);
 	}
 	
 	// Update is called once per frame
@@ -15,4 +23,40 @@ public class CameraController : MonoBehaviour
 	{
 	
 	}
+
+	void LateUpdate()
+	{
+		MoveToTarget ();
+		LookAtTarget ();
+	}
+
+	void SetCameraTarget(Transform t)
+	{
+		target = t;
+		if (target != null) 
+		{
+			if (target.GetComponent<BodyController> ()) 
+			{
+				bodCon = target.GetComponent<BodyController> ();
+			} 
+			else
+				Debug.LogError ("Camera's Target needs a BodyController");
+		} 
+		else
+			Debug.LogError ("Need Camera Target");
+	}
+
+	void MoveToTarget()
+	{
+		destination = bodCon.TargetRotation * offsetFromTarget;
+		destination += target.position;
+		transform.position = destination;
+	}
+
+	void LookAtTarget()
+	{
+		float eulerYAngle = Mathf.SmoothDampAngle (transform.eulerAngles.y, target.eulerAngles.y, ref rotateVel, lookSmooth);
+		transform.rotation = Quaternion.Euler (transform.eulerAngles.x, eulerYAngle, 0);
+	}
+
 }
